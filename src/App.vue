affc1bc49ccbc5027a93b9920ec0d0c2a0880144
Animo <Helmut>
2021-05-19 02:52:07 +0200

    first comit

---
+++ src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+
+    <!-- Foto -->
+    <div class="flex justify-center">
+      <img class="h-28 w-72 mt-24 border-black border" src="./img/foto1.png">
+    </div>
+    
+    
+    <!-- Texto -->
+    
+    <div class="flex">
+      <p class="text-lg font-bold ml-12 mt-4">
+        Studio Location
+        </p>
+    </div>
+     <div class="flex">
+      <p class="text-sm font-thin ml-12">
+      Check our Studio Location  
+       </p>
+    </div>
+
+
+    <!-- Mapa -->
+
+    <div>
+      <div class="flex justify-center mt-6">
+        <img class="w-80 border-black border " src="./img/foto2.jpg">
+      </div>
+    </div>
+    <div class="flex ">
+      <p class="text-medium font-medium ml-8 mt-4">
+        Studio Location
+      </p>
+    </div>
+
+    <!-- Location -->
+    <div class="flex">
+      <img class="ml-8 mt-4 w-6" src="./img/foto3.png">
+      <div class="absolute">
+      <p class="mt-4 ml-16 text-medium">
+        Carl-Reuther-Str. 1 - 68305, Mannheim
+      </p>
+      </div>
+    </div>
+
+    <!-- Hours -->
+     <div class="flex mt-8">
+      <div class="ml-8 font-medium">Our hours</div>
+      <div class="absolute">
+         <img class="ml-10 mt-10 w-6 m-0" src="./img/foto4.png">
+      </div>
+      <div class="absolute">
+        <p class="mt-10 ml-20 text-medium">
+           Mon - Fr 17:00 - 22:00
+         </p>
+      </div>
+      
+    </div>
+  
+  
+  </div>
+</template>
+
+
+<style>
+
+</style>
